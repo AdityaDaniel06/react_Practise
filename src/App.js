55b7496ee4472ac95ff6b3c27f4de1aae1eba05a
AdityaDaniel06 <danieladitya327@gmail.com>
2024-07-07 16:57:15 +0530

    react using create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Pizza />
+
+      <Footer />
     </div>
   );
 }
 
+function Pizza() {
+  return (
+    <div>
+      {" "}
+      <Menu />
+    </div>
+  );
+}
+function Header() {
+  return <h2>Fast React Pizza Co.</h2>;
+}
+function Menu() {
+  return (
+    <div>
+      <h3>Our Pizza Menu</h3>
+      <ul>
+        <li>Margherita</li>
+        <li>Pepperoni</li>
+        <li>Hawaiian</li>
+        <li>Veggie</li>
+      </ul>
+    </div>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpenHour = hour >= openHour && hour <= closeHour;
+  // if (hour >= openHour && hour <= closeHour) {
+  //   alert("We are currently open!!");
+  // } else {
+  //   alert("We are currently closed!!");
+  // }
+  // return React.createElement("footer", null, "We are currently OPEN!!!"); under the hood this is happening
+  return (
+    <footer>
+      {new Date().toLocaleTimeString()}
+      <p>We are currently OPEN!!!</p>
+    </footer>
+  );
+}
 export default App;
